46d27017e581e43a0d8d0c53ec003caee4da4ded
Zamani <azacky302@gmail.com>
2025-01-11 04:09:38 +0700

    Kedua

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb://localhost/uangku'), AuthModule, TransactionsModule, CategoriesModule,],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Load .env file and make it global
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'), // Get MONGO_URI from .env
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    TransactionsModule,
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    // Enable validation globally
-    app.useGlobalPipes(new ValidationPipe({
+  // Enable validation globally
+  app.useGlobalPipes(
+    new ValidationPipe({
       whitelist: true, // Automatically strip properties that do not have decorators
       forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
       transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
-    }));
+    }),
+  );
 
-    app.enableCors({
-      origin: 'http://localhost:8080', // Ganti dengan URL frontend Anda
-      credentials: true, // Jika diperlukan
-    });
-  
-    
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:8080', // Ganti dengan URL frontend Anda
+    credentials: true, // Jika diperlukan
+  });
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Use ConfigService to get PORT
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
